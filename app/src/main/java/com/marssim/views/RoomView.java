982a29042033d794d946dce7f59867f15c448a03
Daniel Wlazło <wlazlo.daniel@gmail.com>
2016-04-24 00:55:14 +0200

    Switching between activities implemented

--- app/src/main/java/com/marssim/views/RoomView.java
+++ app/src/main/java/com/marssim/views/RoomView.java
@@ -1,22 +1,56 @@
 package com.marssim.views;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.content.Intent;
+import android.view.View;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.marssim.Astronaut;
+import com.marssim.Property;
+import com.marssim.R;
 
 
 /**
  * Created by pg on 23/04/16.
  */
-public class RoomView extends LinearLayout {
-    public RoomView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-//        LayoutInflater inflater = LayoutInflater.from(context);
-//        inflater.inflate(R.layout.room_layout, this);
-//        TextView roomDesc = (TextView) f/indViewById(R.id.room_description);
-//        roomDesc.setText("room desc");
+
+
+
+public class RoomView extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.room_layout);
+
+        Button btnReturn = (Button) findViewById(R.id.buttonReturn);
+
+        Intent i = getIntent();
+        // Receiving the Data
+        String name = i.getStringExtra("name");
+        String email = i.getStringExtra("email");
+        Log.e("Second Screen", name + "." + email);
+
+        // Binding Click event to Button
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Closing SecondScreen Activity
+                finish();
+            }
+        });
+
     }
 }

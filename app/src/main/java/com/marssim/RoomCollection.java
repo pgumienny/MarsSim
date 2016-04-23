31199cf52f92bed6d0140b60a82b8863a43e1854
pgumienny <pgumienny@gmail.com>
2016-04-23 13:28:09 +0200

    created collection of rooms and blank rooms

---
+++ app/src/main/java/com/marssim/RoomCollection.java
@@ -0,0 +1,42 @@
+package com.marssim;
+
+import com.marssim.rooms.AbstractRoom;
+import com.marssim.rooms.Bedroom;
+import com.marssim.rooms.EntertainmentRoom;
+import com.marssim.rooms.Greenhouse;
+import com.marssim.rooms.Gym;
+import com.marssim.rooms.Laboratory;
+import com.marssim.rooms.SolarPanels;
+import com.marssim.rooms.Workshop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pg on 23/04/16.
+ */
+public class RoomCollection {
+    public List<AbstractRoom> rooms;
+
+    public RoomCollection() {
+        rooms = new ArrayList<>();
+        rooms.add(new Bedroom());
+        rooms.add(new EntertainmentRoom());
+        rooms.add(new Greenhouse());
+        rooms.add(new Gym());
+        rooms.add(new Laboratory());
+        rooms.add(new SolarPanels());
+        rooms.add(new Workshop());
+    }
+
+    public void passTime(BaseResources baseResources, int time) {
+        for (AbstractRoom room : rooms) {
+            room.getResourcesChange(baseResources, time);
+        }
+    }
+
+    public List<AbstractRoom> getRooms() {
+        return rooms;
+    }
+
+}
---
+++ app/src/main/java/com/marssim/rooms/Workshop.java
@@ -0,0 +1,24 @@
+package com.marssim.rooms;
+
+import com.marssim.Astronaut;
+import com.marssim.BaseResources;
+
+/**
+ * Created by pg on 23/04/16.
+ */
+public class Workshop extends AbstractRoom {
+    @Override
+    public void getResourcesChange(BaseResources baseResources, int timeTicksPassed) {
+
+    }
+
+    @Override
+    public int doAction(Astronaut astronaut) {
+        return 0;
+    }
+
+    @Override
+    public int upgrade(Astronaut astronaut, BaseResources resources) {
+        return 0;
+    }
+}

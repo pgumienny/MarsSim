c8ae0b5c738c760d634ac36d0bf68d8bc881bad5
pgumienny <pgumienny@gmail.com>
2016-04-23 14:56:43 +0200

    RoomView.java

---
+++ app/src/main/java/com/marssim/views/RoomView.java
@@ -0,0 +1,16 @@
+package com.marssim.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by pg on 23/04/16.
+ */
+public class RoomView extends TextView {
+    public RoomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setText("dupa");
+    }
+}
